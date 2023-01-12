ecb09248b379ce28c7a5e0d12b119fb363ef2c24
Danielle Stone Awounang <stonetemfack@gmail.com>
2023-01-12 03:00:16 +0100

    formulaire de connexion

---
+++ Gerer_mon_stock/Gerer_mon_stock/PL/form_connexion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gerer_mon_stock.PL
+{
+    public partial class form : Form
+    {
+        public form()
+        {
+            InitializeComponent();
+        }
+
+        private void form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            //quitons du formulaire
+            this.Close();
+        }
+    }
+}
