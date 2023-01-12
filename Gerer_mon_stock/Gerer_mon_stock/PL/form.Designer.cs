8ee0c0b18adc0aeb3ed7e1c3b3da4eb7e618a787
Danielle Stone Awounang <stonetemfack@gmail.com>
2023-01-12 03:30:41 +0100

    formulaire de  connexion

---
+++ Gerer_mon_stock/Gerer_mon_stock/PL/form.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Gerer_mon_stock.PL
+{
+    partial class form_connexion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_nom_user = new System.Windows.Forms.TextBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.txt_mot_de_passe = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_connexion = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Maroon;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 409);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(364, 2);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Maroon;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(362, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(2, 409);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Maroon;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(2, 409);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Maroon;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(2, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(360, 2);
+            this.panel3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(95, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Connexion";
+            // 
+            // txt_nom_user
+            // 
+            this.txt_nom_user.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.txt_nom_user.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_nom_user.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nom_user.ForeColor = System.Drawing.Color.Silver;
+            this.txt_nom_user.Location = new System.Drawing.Point(81, 154);
+            this.txt_nom_user.Name = "txt_nom_user";
+            this.txt_nom_user.Size = new System.Drawing.Size(243, 22);
+            this.txt_nom_user.TabIndex = 4;
+            this.txt_nom_user.Text = "Nom d\'utilisateur";
+            this.txt_nom_user.Enter += new System.EventHandler(this.txt_nom_user_Enter);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Location = new System.Drawing.Point(84, 178);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(240, 1);
+            this.panel5.TabIndex = 5;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.White;
+            this.panel6.Location = new System.Drawing.Point(84, 248);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(240, 1);
+            this.panel6.TabIndex = 7;
+            // 
+            // txt_mot_de_passe
+            // 
+            this.txt_mot_de_passe.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.txt_mot_de_passe.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_mot_de_passe.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_mot_de_passe.ForeColor = System.Drawing.Color.Silver;
+            this.txt_mot_de_passe.Location = new System.Drawing.Point(84, 227);
+            this.txt_mot_de_passe.Name = "txt_mot_de_passe";
+            this.txt_mot_de_passe.Size = new System.Drawing.Size(243, 22);
+            this.txt_mot_de_passe.TabIndex = 6;
+            this.txt_mot_de_passe.Text = "Mot de passe";
+            this.txt_mot_de_passe.Enter += new System.EventHandler(this.txt_mot_de_passe_Enter);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Gerer_mon_stock.Properties.Resources.password_24px;
+            this.pictureBox2.Location = new System.Drawing.Point(48, 227);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(27, 34);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Gerer_mon_stock.Properties.Resources.user_24px;
+            this.pictureBox1.Location = new System.Drawing.Point(48, 157);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(27, 34);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_close
+            // 
+            this.btn_close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_close.FlatAppearance.BorderSize = 0;
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_close.Image = global::Gerer_mon_stock.Properties.Resources.close_window_48px;
+            this.btn_close.Location = new System.Drawing.Point(317, 8);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(39, 23);
+            this.btn_close.TabIndex = 2;
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // btn_connexion
+            // 
+            this.btn_connexion.BackColor = System.Drawing.Color.Maroon;
+            this.btn_connexion.FlatAppearance.BorderSize = 0;
+            this.btn_connexion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_connexion.Font = new System.Drawing.Font("Lucida Calligraphy", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_connexion.ForeColor = System.Drawing.Color.White;
+            this.btn_connexion.Location = new System.Drawing.Point(48, 324);
+            this.btn_connexion.Name = "btn_connexion";
+            this.btn_connexion.Size = new System.Drawing.Size(276, 39);
+            this.btn_connexion.TabIndex = 10;
+            this.btn_connexion.Text = "Se connecter";
+            this.btn_connexion.UseVisualStyleBackColor = false;
+            // 
+            // form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(364, 411);
+            this.Controls.Add(this.btn_connexion);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.txt_mot_de_passe);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.txt_nom_user);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "form";
+            this.Load += new System.EventHandler(this.form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_nom_user;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.TextBox txt_mot_de_passe;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btn_connexion;
+    }
+}
---
+++ Gerer_mon_stock/Gerer_mon_stock/PL/form_connexion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gerer_mon_stock.PL
+{
+    public partial class form_connexion : Form
+    {
+        public form_connexion()
+        {
+            InitializeComponent();
+        }
+
+        private void form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            //quitons du formulaire
+            this.Close();
+        }
+
+        private void txt_nom_user_Enter(object sender, EventArgs e)
+        {
+            if (txt_nom_user.Text == "Nom d'utilisateur")
+            {
+                txt_nom_user.Text = "";
+                txt_nom_user.ForeColor = Color.White;
+            }
+            else if (txt_mot_de_passe.Text == "")
+            {
+                txt_mot_de_passe.Text = "Mot de passe";
+                txt_mot_de_passe.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txt_mot_de_passe_Enter(object sender, EventArgs e)
+        {
+            if (txt_mot_de_passe.Text == "Mot de passe")
+            {
+                txt_mot_de_passe.Text = "";
+                txt_mot_de_passe.ForeColor = Color.White;
+            }
+            else if(txt_mot_de_passe.Text == "")
+            {
+                txt_mot_de_passe.Text = "Mot de passe";
+                txt_mot_de_passe.ForeColor = Color.Silver;
+            }
+        }
+    }
+}
